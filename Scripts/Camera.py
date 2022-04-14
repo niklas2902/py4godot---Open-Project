8e816a5895c8edc296c2cc3c5a715a2b12ce9166
Niklas <nzimmer@stud.hs-heilbronn.de>
2022-04-14 19:10:35 +0200

    Make camera move with player

--- Scripts/Camera.py
+++ Scripts/Camera.py
@@ -3,13 +3,11 @@ from py4godot.core import *
 from py4godot.classes.generated import *
 from py4godot.pluginscript_api.utils.annotations import *
 
-DEFAULT_OFFSET = 5
 @gdclass
 class PlayerCam(Camera):
 	def __init__(self):
 		super().__init__()
 		self._player_path = None
-		self._y_offset = DEFAULT_OFFSET
 	
 	@gdproperty(NodePath, NodePath())
 	def player_path(self):
@@ -18,19 +16,13 @@ class PlayerCam(Camera):
 	def player_path(self, value):
 		self._player_path = value
 	
-	@gdproperty(int, DEFAULT_OFFSET, hint=PropertyHint.GODOT_PROPERTY_HINT_RANGE.value, hint_string="1,20,1,slider")
-	def y_offset(self):
-		return self._y_offset
-	@y_offset.setter
-	def y_offset(self, value):
-		self._y_offset = value
-
 	@gdmethod
 	def _ready(self):
 		print("_init Camera")
 		self.player = KinematicBody.cast(self.get_node(self.player_path))
+		self.start_pos = self.transform.get_origin() - self.player.transform.get_origin()
+		self.start_pos.set_axis(0,0)
 
 	@gdmethod
 	def _process(self, delta):
-		self.transform.set_origin(self.player.transform.get_origin() + Vector3(0,self.y_offset,0))
-
+		self.transform.set_origin(self.player.transform.get_origin() + self.start_pos)
