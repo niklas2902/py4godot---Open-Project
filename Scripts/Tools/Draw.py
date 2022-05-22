4699f4408a3ca46b35a73b6d3d536707b678b155
Niklas <nzimmer@stud.hs-heilbronn.de>
2022-05-22 11:53:25 +0200

    Allow drawing sphere

--- Scripts/Tools/Draw.py
+++ Scripts/Tools/Draw.py
@@ -6,6 +6,7 @@ from py4godot.pluginscript_api.utils.annotations import *
 from py4godot.pluginscript_api.hints import *
 import math
 
+RESOLUTION = 20
 @gdtool
 class Draw(ImmediateGeometry):
 
@@ -18,7 +19,7 @@ class Draw(ImmediateGeometry):
 		print("draw")
 		self.clear()
 		self.begin(1, ImageTexture._new());
-		num_points = 50
+		num_points = RESOLUTION
 		alpha = 2*math.pi *(num_points-1) / num_points
 		vector_before = Vector3(math.cos(alpha),0, math.sin(alpha)) * rad + Vector3(0,self.transform.get_origin().get_axis(Vector3_Axis.Y.value),0)
 		for alpha_part in range(0,num_points):
@@ -30,8 +31,52 @@ class Draw(ImmediateGeometry):
 				self.add_vertex(vector_before)
 			vector_before = Vector3(x,0,y) * rad +  Vector3(0,self.transform.get_origin().get_axis(Vector3_Axis.Y.value),0)
 		self.end()
-	
+
+	def draw_sphere(self, rad):
+		print("draw")
+		self.clear()
+		self.begin(1, ImageTexture._new());
+		num_points = RESOLUTION
+		alpha = 2*math.pi *(num_points-1) / num_points
+		vectors = []
+		vector_before = Vector3(math.cos(alpha),0, math.sin(alpha)) * rad
+		for alpha_part in range(0,num_points):
+			alpha = 2*math.pi *alpha_part / num_points
+			x = math.cos(alpha)
+			y = math.sin(alpha)
+			vectors.append(Vector3(x,0,y) * rad )
+			if(vector_before):
+				vectors.append(vector_before)
+			vector_before = Vector3(x,0,y) * rad
+
+		z_rotation = []
+		for vector in vectors:
+			z_rotation.append(vector.rotated(Vector3.FORWARD, math.pi/2))
+
+		x_rotation = []
+		for vector in vectors:
+			x_rotation.append(vector.rotated(Vector3.RIGHT, math.pi/2))
+		for vector in vectors + z_rotation + x_rotation:
+			self.add_vertex(vector + Vector3(0,self.transform.get_origin().get_axis(Vector3_Axis.Y.value),0))
+
+		self.end()
+
+	def draw_ray(self, origin, direction, length):
+		self.clear()
+		self.begin(1, ImageTexture._new());
+		self.add_vertex(origin)
+		self.add_vertex(origin + direction * length)
+		self.end()
+
+	def draw_line(self, origin, target):
+		self.clear()
+		self.begin(1, ImageTexture._new());
+		self.add_vertex(origin)
+		self.add_vertex(target)
+		self.end()
+		
 	@gdmethod
 	def _physics_process(self, delta):
-		self.draw_cirlce(2)
+		#self.draw_cirlce(2)
+		self.draw_sphere(2)
 
