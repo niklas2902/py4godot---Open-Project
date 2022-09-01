8fec541ef8a6ba2e1c42d98be750d4bd9b368bb2
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-09-01 21:30:26 +0200

    Furhter implement AStar

--- Scripts/Navigation/AStar.py
+++ Scripts/Navigation/AStar.py
@@ -15,10 +15,23 @@ class AStar(Spatial):
 	def _ready(self):
 		self.astar = AStar._new()
 		self.walkables = self.get_tree().get_nodes_in_group(WALKABLE_GROUP)
+		self.generate_points()
 
 	def generate_points(self)->None:
 		"""Here we are generating all the points we could later use for astar"""
-		pass
+		for walkable in self.walkables:
+			mesh: Optional[MeshInstance] = self.get_mesh(walkable)
+			if (not mesh):
+				continue
+			aabb: AABB = mesh.get_transformed_aabb()
+			print(aabb)
+
+	def get_mesh(self, node:Node) -> Optional[CSGMesh]:
+		for child in node.get_children():
+			print("Node:", child)
+			if isinstance(child, CSGMesh):
+				print("return")
+				return child
 
 	def generate_point_connections(self)->None:
 		"""Here we generate the connections between the points, the player can walk"""
