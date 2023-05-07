ccb4f07fdcb356f8320f0bb6691995f092eb600b
niklas <niklaszimmer2902@gmail.com>
2023-05-07 22:34:10 +0200

    Fixed import problem

--- Scripts/BehaviorTree/Blackboard.py
+++ Scripts/BehaviorTree/Blackboard.py
@@ -1,7 +1,9 @@
 from __future__ import annotations  # Without this, the type hint below would not work.
 
+import Scripts.Enemy as enemy
+
 
 class Blackboard:
-    enemy: object
-    def __init__(self, enemy:object):
+    enemy: enemy.Enemy
+    def __init__(self, enemy:enemy.Enemy):
         self.enemy = enemy
