8073ce4e104a3bf177467489663cf129f409d578
niklas <niklaszimmer2902@gmail.com>
2023-09-10 20:14:20 +0200

    Updated start_godot.py

--- .vscode/start_godot.py
+++ .vscode/start_godot.py
@@ -2,7 +2,7 @@ import sys, subprocess,time, socket, debugpy,os
 print("start godot")
 if(os.path.exists("locked_debug")):
     os.remove("locked_debug")
-res = subprocess.Popen('"C:/Users/nikla/Downloads/Godot_v3.5.1-stable_win64.exe(1)/Godot_v3.5.1-stable_win64.exe" --path C:/Users/nikla/OneDrive/Dokumente/repositories/py4godot---Open-Project',
+res = subprocess.Popen('cmd start /K godot.exe --path C:/Users/nikla/OneDrive/Dokumente/repositories/py4godot---Open-Project',
                         shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP|subprocess.DETACHED_PROCESS)
 
 while True:
