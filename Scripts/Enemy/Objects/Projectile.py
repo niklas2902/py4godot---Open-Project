f969a02ba7cf1ac9e0ef53d2e2fe0d1815d055b5
Niklas Zimmer <nzimmer@uni-bremen.de>
2023-11-01 21:19:06 +0100

    Projectile now deleted when hitting wall

--- Scripts/Enemy/Objects/Projectile.py
+++ Scripts/Enemy/Objects/Projectile.py
@@ -19,13 +19,19 @@ class Projectile(Spatial):
     spawner: Spawner
     stopped: bool
 
+    utils: Node
+    utils_path: NodePath
+
     prop("lifetime", float, 10)
+    prop("utils_path", NodePath, NodePath())
     register_signal("lifetime_over")
 
     @gdmethod
     def _ready(self) -> None:
         self._current_lifetime_counter = 0
         self.velocity = Vector3(1, 0, 0)
+
+    # self.utils = self.get_node(self.utils_path)
 
     @gdmethod
     def _process(self, delta: float) -> None:
@@ -35,6 +41,19 @@ class Projectile(Spatial):
                 self.spawner.lifetime_over(self)
             else:
                 self.global_transform.set_origin(self.global_transform.get_origin() + self.velocity * delta)
+
+    @gdmethod
+    def _physics_process(self, delta: float) -> None:
+        hitting_wall:bool = self.is_hitting_wall()
+        if hitting_wall:
+            self.spawner.lifetime_over(self)
+
+    def is_hitting_wall(self):
+        res = self.utils.callv("sphere_cast",
+                               Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
+                                     4)).get_converted_value()
+        print(res.size(), res.size() != 0)
+        return res.size() != 0
 
     def reset_lifetime(self) -> None:
         self._current_lifetime_counter = 0
--- Scripts/Enemy/Objects/Spawner.py
+++ Scripts/Enemy/Objects/Spawner.py
@@ -11,63 +11,64 @@ MAX_BULLETS = 10
 
 @gdclass
 class Spawner(StaticBody):
-    prop("utils_path", NodePath, NodePath())
-    prop("reload_time", float, 0, hint=RangeHint(0, 10, is_slider=True))
+	prop("utils_path", NodePath, NodePath())
+	prop("reload_time", float, 0, hint=RangeHint(0, 10, is_slider=True))
 
-    reload_time: float
-    _current_time: float = 0
+	reload_time: float
+	_current_time: float = 0
 
-    utils_path: NodePath
-    projectile_scene: PackedScene
-    demo_projectile: Projectile
+	utils_path: NodePath
+	projectile_scene: PackedScene
+	demo_projectile: Projectile
 
-    projectiles_list: typing.List[Projectile]
-    current_projectile_id = 0
-    free_instances: int
+	projectiles_list: typing.List[Projectile]
+	current_projectile_id = 0
+	free_instances: int
 
-    @gdmethod
-    def _ready(self) -> None:
-        # ResourceLoader.instance().load("res://Prefabs/Projectile.tscn")
-        self.utils = self.get_node(self.utils_path)
+	@gdmethod
+	def _ready(self) -> None:
+		# ResourceLoader.instance().load("res://Prefabs/Projectile.tscn")
+		self.utils = self.get_node(self.utils_path)
 
-        self.projectile_scene: PackedScene = typing.cast(
-            PackedScene,
-            self.utils.callv("load_resource", Array("res://Prefabs/Projectile.tscn")).get_converted_value())
-        self.demo_projectile = typing.cast(Projectile, self.projectile_scene.instance().get_pyscript())
-        # self.add_child(self.demo_projectile)
+		self.projectile_scene: PackedScene = typing.cast(
+			PackedScene,
+			self.utils.callv("load_resource", Array("res://Prefabs/Projectile.tscn")).get_converted_value())
+		self.demo_projectile = typing.cast(Projectile, self.projectile_scene.instance().get_pyscript())
+		# self.add_child(self.demo_projectile)
 
-        self.projectiles_list = []
-        for i in range(MAX_BULLETS):
-            self.projectiles_list.append(self.demo_projectile.duplicate())
-            self.projectiles_list[-1].get_pyscript().lifetime = 10
-            self.projectiles_list[-1].get_pyscript().spawner = self
-        self.free_instances = MAX_BULLETS
+		self.projectiles_list = []
+		for i in range(MAX_BULLETS):
+			self.projectiles_list.append(self.demo_projectile.duplicate())
+			self.projectiles_list[-1].get_pyscript().lifetime = 10
+			self.projectiles_list[-1].get_pyscript().spawner = self
+		self.free_instances = MAX_BULLETS
 
-    @gdmethod
-    def _physics_process(self, delta: float) -> None:
-        self._current_time += delta
-        if self._current_time > self.reload_time:
-            self._current_time = 0
-            self.spawn_projectile()
+	@gdmethod
+	def _physics_process(self, delta: float) -> None:
+		self._current_time += delta
+		if self._current_time > self.reload_time:
+			self._current_time = 0
+			self.spawn_projectile()
 
-    @gdmethod
-    def _process(self, delta: float) -> None:
-        pass
+	@gdmethod
+	def _process(self, delta: float) -> None:
+		pass
 
-    @gdmethod
-    def lifetime_over(self, projectile: Projectile) -> None:
-        print("lifetime_over")
-        typing.cast(Projectile, projectile.get_pyscript()).global_transform.set_origin(
-            self.global_transform.get_origin())
-        projectile.reset_lifetime()
-        self.remove_child(Node.cast(projectile))
-        self.free_instances += 1
+	@gdmethod
+	def lifetime_over(self, projectile: Projectile) -> None:
+		print("lifetime_over")
+		typing.cast(Projectile, projectile.get_pyscript()).global_transform.set_origin(
+			self.global_transform.get_origin())
+		projectile.reset_lifetime()
+		self.remove_child(Node.cast(projectile))
+		self.free_instances += 1
 
-    def spawn_projectile(self) -> None:
-        if self.current_projectile_id < len(self.projectiles_list) and self.free_instances > 0:
-            # self.projectiles_list[self.current_projectile_id].get_pyscript().connect("lifetime_over", self, "lifetime_over")
-            self.add_child(self.projectiles_list[self.current_projectile_id])
-            typing.cast(Projectile, self.projectiles_list[self.current_projectile_id].get_pyscript()).reset_lifetime()
-            self.current_projectile_id += 1
-            self.current_projectile_id %= len(self.projectiles_list)
-            self.free_instances -= 1
+	def spawn_projectile(self) -> None:
+		if self.current_projectile_id < len(self.projectiles_list) and self.free_instances > 0:
+			# self.projectiles_list[self.current_projectile_id].get_pyscript().connect("lifetime_over", self, "lifetime_over")
+			self.add_child(self.projectiles_list[self.current_projectile_id])
+			typing.cast(Projectile, self.projectiles_list[self.current_projectile_id].get_pyscript()).reset_lifetime()
+			typing.cast(Projectile, self.projectiles_list[self.current_projectile_id].get_pyscript()).utils = self.utils
+			self.current_projectile_id += 1
+			self.current_projectile_id %= len(self.projectiles_list)
+			self.free_instances -= 1
