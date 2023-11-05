9a366520ddf47eaf3cd06c391eba44480ec75abc
Niklas Zimmer <nzimmer@uni-bremen.de>
2023-11-06 00:02:34 +0100

    Projectile now deleted when hitting player

--- Scripts/Enemy/Objects/Projectile.py
+++ Scripts/Enemy/Objects/Projectile.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from Scripts.Enemy.Objects.Spawner import Spawner
+	from Scripts.Enemy.Objects.Spawner import Spawner
 from py4godot.enums.enums import *
 from py4godot.core import *
 from py4godot.classes.generated import *
@@ -12,48 +12,58 @@ from py4godot.pluginscript_api.hints import *
 
 @gdclass
 class Projectile(Spatial):
-    velocity: Vector3
+	velocity: Vector3
 
-    lifetime: float
-    _current_lifetime_counter: float
-    spawner: Spawner
-    stopped: bool
+	lifetime: float
+	_current_lifetime_counter: float
+	spawner: Spawner
+	stopped: bool
 
-    utils: Node
-    utils_path: NodePath
+	utils: Node
+	utils_path: NodePath
 
-    prop("lifetime", float, 10)
-    prop("utils_path", NodePath, NodePath())
-    register_signal("lifetime_over")
+	prop("lifetime", float, 10)
+	prop("utils_path", NodePath, NodePath())
+	register_signal("lifetime_over")
 
-    @gdmethod
-    def _ready(self) -> None:
-        self._current_lifetime_counter = 0
-        self.velocity = Vector3(1, 0, 0)
+	@gdmethod
+	def _ready(self) -> None:
+		self._current_lifetime_counter = 0
+		self.velocity = Vector3(1, 0, 0)
 
-    # self.utils = self.get_node(self.utils_path)
+	# self.utils = self.get_node(self.utils_path)
 
-    @gdmethod
-    def _process(self, delta: float) -> None:
-        if self.is_inside_tree():
-            self._current_lifetime_counter += delta
-            if (self._current_lifetime_counter > self.lifetime):
-                self.spawner.lifetime_over(self)
-            else:
-                self.global_transform.set_origin(self.global_transform.get_origin() + self.velocity * delta)
+	@gdmethod
+	def _process(self, delta: float) -> None:
+		if self.is_inside_tree():
+			self._current_lifetime_counter += delta
+			if (self._current_lifetime_counter > self.lifetime):
+				self.spawner.lifetime_over(self)
+			else:
+				self.global_transform.set_origin(self.global_transform.get_origin() + self.velocity * delta)
 
-    @gdmethod
-    def _physics_process(self, delta: float) -> None:
-        hitting_wall:bool = self.is_hitting_wall()
-        if hitting_wall:
-            self.spawner.lifetime_over(self)
+	@gdmethod
+	def _physics_process(self, delta: float) -> None:
+		hitting_wall: bool = self.is_hitting_wall()
+		if hitting_wall:
+			self.spawner.lifetime_over(self)
 
-    def is_hitting_wall(self):
-        res = self.utils.callv("sphere_cast",
-                               Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
-                                     4)).get_converted_value()
-        print(res.size(), res.size() != 0)
-        return res.size() != 0
+		hitting_player: bool = self.is_hitting_player()
+		if hitting_player:
+			print("hitting player")
+			self.spawner.lifetime_over(self)
 
-    def reset_lifetime(self) -> None:
-        self._current_lifetime_counter = 0
+	def is_hitting_wall(self) -> None:
+		res = self.utils.callv("sphere_cast",
+							   Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
+									 4)).get_converted_value()
+		return res.size() != 0
+
+	def is_hitting_player(self) -> None:
+		res = self.utils.callv("sphere_cast",
+							   Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
+									 1)).get_converted_value()
+		return res.size() != 0
+
+	def reset_lifetime(self) -> None:
+		self._current_lifetime_counter = 0
