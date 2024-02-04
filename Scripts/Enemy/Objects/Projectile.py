633197b5cf78ab9c879e9f822f122fc73ed29690
Niklas Zimmer <niklas-zimmer@hotmail.com>
2024-02-04 17:38:09 +0100

    Projectiles should now be able to collide

--- Scripts/Enemy/Objects/Projectile.py
+++ Scripts/Enemy/Objects/Projectile.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from py4godot.classes.Node.Node import Node
 from py4godot.classes.Node3D.Node3D import Node3D
-from py4godot.classes.generated4_core import Vector3, NodePath, Array
+from py4godot.classes.PhysicsShapeQueryParameters3D.PhysicsShapeQueryParameters3D import PhysicsShapeQueryParameters3D
+from py4godot.classes.SphereShape3D.SphereShape3D import SphereShape3D
+from py4godot.classes.generated4_core import Vector3, NodePath, Array, Transform3D
 
 from Scripts.CharHandler import CharHandler
 from typing import TYPE_CHECKING
@@ -10,6 +12,9 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from Scripts.Enemy.Objects.Spawner import Spawner
 from py4godot.pluginscript_api.utils.annotations import *
+
+PUSH_OBJ_MASK: int = 4
+GROUND_MASK: int = 1
 
 
 @gdclass
@@ -62,21 +67,32 @@ class Projectile(Node3D):
                                 Array(self.get_global_position(), 0.1, Array(self.spawner),
                                       1))[0]["collider"].get_pyscript()
 
+    def sphere_cast(self, position, radius, mask=GROUND_MASK) -> Array:
+        shape: SphereShape3D = SphereShape3D.constructor()
+        shape.set_radius(radius)
+
+        params = PhysicsShapeQueryParameters3D.constructor()
+        params.set_shape(shape)
+        if mask != -1:
+            params.collision_mask = mask
+        translated_transform: Transform3D = self.global_transform.translated(position)
+        params.set_transform(translated_transform)  # same transform as parent, just translate
+
+        # array: Array = Array.new0()
+        # array.push_back(self)
+        # params.set_exclude(array)  # here exclude is an array of... RID??
+        res: Array = self.get_world_3d().direct_space_state.intersect_shape(params)
+        if res.size() > 0:
+            pass
+        return res
+
     def is_hitting_wall(self) -> bool:
-        # res = self.utils.callv("sphere_cast",
-        #                       Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
-        #                             4)).get_converted_value()
-
-        # return res.size() != 0
-
-        return False
+        res = self.sphere_cast(self.global_position, 0.1, 4)
+        return res.size() != 0
 
     def is_hitting_player(self) -> bool:
-        # res = self.utils.callv("sphere_cast",
-        #                       Array(self.global_transform.get_origin(), 0.1, Array(self.spawner),
-        #                             1)).get_converted_value()
-        # return res.size() != 0
-        return False
+        res = self.sphere_cast(self.global_position, 0.1, 1)
+        return res.size() != 0
 
     def reset_lifetime(self) -> None:
         self._current_lifetime_counter = 0
