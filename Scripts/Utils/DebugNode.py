ebf944fc397d62f0e4b5244ed95dfcff46c9a1dc
Niklas Zimmer <niklas-zimmer@hotmail.com>
2024-03-11 16:58:56 +0100

    Updated files

--- Scripts/Utils/DebugNode.py
+++ Scripts/Utils/DebugNode.py
@@ -1,12 +1,21 @@
-# import debugpy
-from py4godot.classes.Node.Node import Node
+import debugpy
 
-from py4godot.pluginscript_api.utils.annotations import *
+from py4godot.classes.Node.Node import Node
+from py4godot.pluginscript_api.utils.annotations import gdclass
 
 
 @gdclass
 class DebugNode(Node):
 
-	@gdmethod
 	def _enter_tree(self) -> None:
-		pass
+		try:
+			with open("locked_debug", "w"):
+				pass
+			debugpy.log_to('log.txt')
+			debugpy.configure(
+				python=r"C:\Users\nikla\OneDrive\Dokumente\repositories\py4godot---Open-Project\addons\windows64\cpython-3.9.7-windows64\python\install\python.exe")
+			debugpy.listen(("localhost", 5678))
+			print("before wait for client")
+			debugpy.wait_for_client()  # blocks execution until client is attached
+		except Exception as e:
+			print("Exception:", e)
